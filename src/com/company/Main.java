b16eec1ed4dd8abf281b4d6acfe5e5164a9d0630
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2017-08-31 23:25:01 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+              byte aAlpha = 127; //A signed integeger to save memory
+              short bBravo = 16;  //A signed integer used also to save memory is 16 bit
+              int cCharlie = 32; //int is a 32 bit signed integer or compares too
+              long dDelta = 64; //A value of Integer after widening primitive conversion
+              float eEcho = (float) 1.313; //Returns value as float
+                double doubleFoxtrot = 63.36;//A large number with decimal point
+                    boolean gGulf = true;//A value that shows true or false values
+              char hHotel = 385;//A single 16-bit Unicode Character
+
+
+
+        System.out.println("byte: " + aAlpha);
+        System.out.println("short: " + bBravo);
+        System.out.println("int: " + cCharlie);
+        System.out.println("Long: " + dDelta);
+        System.out.println("Float: " + eEcho);
+        System.out.println("Double: " + doubleFoxtrot);
+        System.out.println("Boolean: " + gGulf);
+        System.out.println("char: " + hHotel);
+    }
+}
